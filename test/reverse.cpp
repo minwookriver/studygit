9158d7f85cb1bdae16bae56838acb94ee01cef68
sota-junsik <bbohae@snu.ac.kr>
2023-02-18 08:56:41 +0000

    test

--- test/reverse.cpp
+++ test/reverse.cpp
@@ -3,25 +3,27 @@ using namespace std;
 #define N 10
 
 
-void reverse(int arr[N])
-{
-  int i;
-  for (i=9; i>=0; --i){ // 9부터 0까지 증감 하면서 arr을 출력하는 부분
-    cout << arr[i];
+void reverse2(const int input_array[N], int output_array[N]){
+  for (int i=0; i<10; ++i){
+    output_array[N-i-1] = input_array[i];
   }
 }
-int main()
-{
-  int arr[N];
+
+
+int main(){
+  int arr1[N];
+  int arr2[N];
   int i;
   for (i=0; i<N; ++i){ // arr을 0부터 9까지  입력 받는부분
-    cin >> arr[i];
+  //  cin >> arr1[i];
   }
-  reverse(arr);
-  cout << endl;
+  printf("%p\n", arr1);
+  printf("%d\n", arr1[0]);
+  printf("%p\n", arr1+9);
+  
+
+//  reverse2(arr1, arr2);
+ 
   return 0;
 }
-/* reverse 9째줄 반복문 조건에 i=10을하면 garbage value뱉음
-   arr[0]~arr[9]까지  arr[10]은 없음
 
-*/
--- test/transpose.cpp
+++ test/transpose.cpp
@@ -1,26 +1,48 @@
 #include<iostream>
 using namespace std;
 
-void transpose(const int input)
-{
-int i,j;  // TODO : 행과 열을 바꾸는 부분
-  for (j=0; j<4; ++j){
-    for (i=0; i<2; ++i){
-      cout << input[i][j];
+#define WIDTH 4
+#define HEIGHT 2
+
+void print_array(int arr[][WIDTH], int W, int H){
+  for (int h=0; h<H; ++h){
+    for (int w=0; w<W; ++w){
+      cout << arr[h][w];
+    }
+    cout << endl;
+  }
+}
+
+void print_array2(int arr[][HEIGHT], int W, int H){
+  for (int h=0; h<H; ++h){
+    for (int w=0; w<W; ++w){
+      cout << arr[h][w];
+    }
+    cout << endl;
+  }
+}
+
+void transpose(const int input[HEIGHT][WIDTH], int output[WIDTH][HEIGHT]){
+  for (int h=0; h<HEIGHT; ++h){
+    for (int w=0; w<WIDTH; ++w){
+      output[w][h] = input[h][w];
     }
   }
 }
-int main()
-{
- const int input[2][4];
- int i,j;
-  // TODO : 입력받는부분
-   for (i=0; i<2; ++i){
-     for (j=0; j<4; ++j){
-       cin >> input[i][j];
-     }
-   }
-  transpose(input);
-  cout << endl;
+
+int main(){
+  int input[HEIGHT][WIDTH];
+  int output[WIDTH][HEIGHT];
+  
+  for (int h=0; h<HEIGHT; ++h){
+    for (int w=0; w<WIDTH; ++w){
+      cin >> input[h][w];
+    }
+  }
+
+  transpose(input, output);
+  print_array(input, WIDTH, HEIGHT);
+  print_array2(output, HEIGHT, WIDTH);
+
   return 0;
 }
